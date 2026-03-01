c867b5cdb370b01cf9678f3630ad1093f1d197a7
antonioappeso90-creator <antonioappeso90@gmail.com>
2026-03-01 16:15:18 +0100

    Create postcss.config.mjs

---
+++ postcss.config.mjs
@@ -0,0 +1,10 @@
+/**
+ * POSTCSS CONFIG - IL PONTE (V1)
+ * MENTORE DOCET: Senza questo file, Next.js non sa cosa farsene di Tailwind 4.
+ * POSIZIONE: Cartella principale (Root).
+ */
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
+};
